c26be5c661963e2533bb82bda0fc03bfd994ec3e
hirrumi <BlueberryRainbows@yandex.ru>
2024-02-29 23:35:58 +0300

    Изменение урлов

--- catalog/urls.py
+++ catalog/urls.py
@@ -11,10 +11,10 @@ urlpatterns = [
     path('', ProductListView.as_view()),
     path('products/<int:pk>', ProductDetailView.as_view(), name='product'),
 
-    path('create/', BlogCreateView.as_view(), name='create'),
+    path('blog_create/', BlogCreateView.as_view(), name='create'),
     path('blog/', BlogListView.as_view(), name='list'),
-    path('detail/<int:pk>', BlogDetailView.as_view(), name='detail'),
-    path('update/<int:pk>', BlogUpdateView.as_view(), name='update'),
+    path('blog_detail/<int:pk>', BlogDetailView.as_view(), name='detail'),
+    path('blog_update/<int:pk>', BlogUpdateView.as_view(), name='update'),
     path('blog/<int:pk>', BlogDeleteView.as_view(), name='delete'),
-    path('activate/<int:pk>', activates, name='activate'),
+    path('blog_activate/<int:pk>', activates, name='activate'),
 ]
